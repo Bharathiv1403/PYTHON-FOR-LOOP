c106a3111ca78a226d25712785baba0d0705dbe9
Shakthi1005 <139037931+Shakthi1005@users.noreply.github.com>
2023-10-26 10:34:49 +0530

    Update nestedForLoop.py

--- nestedForLoop.py
+++ nestedForLoop.py
@@ -1,6 +1,6 @@
 # prints times table from 1 - 10 in nested for loop
 
-for i in range(1, 11):
-    for j in range(1, 11):
+for i in range(1, 21):
+    for j in range(1, 21):
         result = i * j
         print(f"{i} x {j} = {result}")
